2de14f226cbbba0feb348a3ab418e6e7bcee7605
Abdul-Rehman-Devv <awanmani244@gmail.com>
2026-01-14 17:17:24 +0500

    gsap animation

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+function firstPageAnim() {
+    let tl = gsap.timeline();
 
+    tl.from("#nav",{
+        y: '-10',
+        opacity: 0 ,
+        duration:2,
+        ease: Expo.easeInOut 
+
+    })
+}
 function circleMouseFollower(){
     window.addEventListener('mousemove',function(dets){
     document.querySelector('#minicircle').style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`
     })
 }
 circleMouseFollower()
+firstPageAnim()

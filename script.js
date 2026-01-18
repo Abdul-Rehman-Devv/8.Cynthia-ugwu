834b0c700ba3f84da8f1079c45291ce7f8dd5915
Abdul-Rehman-Devv <awanmani244@gmail.com>
2026-01-18 12:18:23 +0500

    Mouse Skewing

--- script.js
+++ script.js
@@ -17,7 +17,16 @@ function firstPageAnim() {
         y:0,
         ease: Expo.easeInOut, 
         duration:2,
+        delay : -1,
         stagger: .2
+    })
+    .from("#mainFooter",{
+        y:-10,
+        opacity: 0,
+        duration: 1.5,
+        delay : -1 ,
+        ease: Expo.easeInOut
+
     })
 }
 function circleMouseFollower(){
@@ -25,5 +34,22 @@ function circleMouseFollower(){
     document.querySelector('#minicircle').style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`
     })
 }
+function circleSkew(){
+    var Xscale = 1
+    var Yscale = 1
+
+    var Xdiff = 0
+    var Ydiff = 0
+    window.addEventListener('mousemove', function(dets){
+        var Xdiff = Xscale - Xprev ;
+        var Ydiff = Xscale - Yprev ;
+        
+        Xprev = dets.clientX;
+        Yprev = dets.clientY;
+        console.log(Xdiff , Ydiff)
+
+    })
+}
+circleSkew()
 circleMouseFollower()
 firstPageAnim()
